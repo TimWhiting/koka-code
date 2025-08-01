ef72a016f5b44a1300af4955f1c92c4d13b2deef
Tim Whiting <tim@whitings.org>
2025-08-01 10:28:46 -0600

    remove old

--- compiler/alex.sh
+++
@@ -1,4 +0,0 @@
-cd ../../koka-community/alex
-stack build
-# stack run alex -- -k ../../koka-code/compiler/syntax/old/koka.x -o ../../koka-code/compiler/syntax/old/koka-lex.kk
-stack run alex -- -k ../../koka-code/compiler/syntax/koka.x -o ../../koka-code/compiler/syntax/lex.kk
